1f03b29b66beca9c20818cd6636502611a717014
Marcos Santos <marcos.diego.ns@outlook.com>
2020-10-26 13:11:42 -0500

    JSON Parsing

--- countries.py
+++ countries.py
@@ -1,7 +1,12 @@
+import json
+
 import requests
 
 URL_ALL = "https://restcountries.eu/rest/v2"
 URL_NAME = "https://restcountries.eu/rest/v2/name/bra"
-answer = requests.get(URL_NAME)
+answer = requests.get(URL_ALL)
 
-print(answer.text)
+countries = json.loads(answer.text)  # JSON parsing to python object
+
+for country in countries:
+    print(country["name"], country["currencies"])
